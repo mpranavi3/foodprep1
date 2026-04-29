90659d67d8e0f41163c8bc5e98d98362384991b6
mpranavi3 <padmapranavimamillapalli@gmail.com>
2026-04-29 11:27:47 +0530

    Update vite.config.js

--- userfrontend/vite.config.js
+++ userfrontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // User frontend at root
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:10000'
+    }
+  }
 })
