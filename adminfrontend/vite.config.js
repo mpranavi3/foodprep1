a509b8de93a380a774490fadc791a9980587e53f
mpranavi3 <padmapranavimamillapalli@gmail.com>
2026-04-29 11:28:17 +0530

    Update vite.config.js

--- adminfrontend/vite.config.js
+++ adminfrontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/admin/',  // Important: admin at /admin path
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': 'http://localhost:10000'
+    }
+  }
 })
